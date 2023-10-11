5a530a0eafc6f73c825c42f61ef80f6daa2ae175
DaneIwema <daneiwema@gmail.com>
2023-10-11 17:58:11 -0500

    Removed Unnecessary saved data and restructured Piece data types

--- Tetris/Piece.java
+++ Tetris/Piece.java
@@ -7,34 +7,35 @@ class Piece {
     private Block [] blocks;
 
     int [] pieceData;
+    //[0] Piece column Value
+    //[1] Piece Type
+    //[2] Piece rotation
+
 
     int [][] blockData;
     //[0]-[3]([0]-[1]) block coordinates
     //[0]-[3]([2]) Is bottom piece or not
-    //[4]([0]-[1]) piece coordinates
-    //[4][2] piece type
-    //[4][3] piece rotation
 
     public Piece() {
-        newPiece(2, 6);
+        newPiece(6);
     }
 
     public int getCol(){
-        return blockData[4][1];
+        return pieceData[0];
     }
 
     public void setCol(int col){
-        blockData[4][1] = col;
+        pieceData[0] = col;
     }
 
     public int getRotation(){
-        return blockData[4][3];
+        return pieceData[2];
     }
 
     public void setRotation(int target){
-        for (int i = blockData[4][3]; i < target + 4; i++)
+        for (int i = pieceData[2]; i < target + 4; i++)
             rotate();
-        blockData[4][3] = target;
+        pieceData[2] = target;
     }
 
     public int getBCol(int slot){
@@ -50,24 +51,24 @@ class Piece {
     }
 
     public void moveRight(){
-        blockData[4][1] = blockData[4][1] + 1;
+        pieceData[0] = pieceData[0] + 1;
     }
 
     public void moveLeft(){
-        blockData[4][1] = blockData[4][1] - 1;
+        pieceData[0] = pieceData[0] - 1;
     }
 
     public void rotate(){
-        if (blockData[4][2] != 1){
+        if (pieceData[1] != 1){
             for (int i = 0; i < 4; i++){
             int column = (blockData[i][0]*(int)Math.cos(Math.toRadians(90)))-(blockData[i][1]*(int)Math.sin(Math.toRadians(90)));
             int row = (blockData[i][0]*(int)Math.sin(Math.toRadians(90)))-(blockData[i][1]*(int)Math.cos(Math.toRadians(90)));
             blockData[i][0] = column;
             blockData[i][1] = row;
             }
-            if (blockData[4][3] == 4)
-                blockData[4][3] = 0;
-            blockData[4][3] = blockData[4][3] + 1;
+            if (pieceData[2] == 4)
+                pieceData[2] = 0;
+            pieceData[2] = pieceData[2] + 1;
             updateBottomPieces();
         }
     }
@@ -95,7 +96,7 @@ class Piece {
     }
 
     public void updateBottomPieces(){
-        switch(blockData[4][2]){
+        switch(pieceData[1]){
             case 0:
             case 1:
                 updateBottomIfIOrCubePiece();
@@ -106,9 +107,9 @@ class Piece {
                 break;
             case 3:
             case 4:
-                if (blockData[4][3] == 1)
+                if (pieceData[2] == 1)
                     updateBottomIfIOrCubePiece();
-                else if (blockData[4][3] == 2 || blockData[4][3] == 4){
+                else if (pieceData[2] == 2 || pieceData[2] == 4){
                     updateBottomIfIOrCubePiece();
                     IfHasWIngs();
                 }
@@ -117,7 +118,7 @@ class Piece {
                 break;
             case 5:
             case 6:
-                if(blockData[4][3] == 1 || blockData[4][3] == 3){
+                if(pieceData[2] == 1 || pieceData[2] == 3){
                     updateBottomIfIOrCubePiece();
                     IfHasWIngs();
                 }
@@ -129,13 +130,13 @@ class Piece {
     }
 
     private void ifLOrJThirdRot(){
-        if(blockData[4][2] == 4){
+        if(pieceData[1] == 4){
             blockData[0][2] = 1;
             blockData[1][2] = 0;
             blockData[2][2] = 1;
             blockData[3][2] = 1;
         }
-        else if (blockData[4][2] == 3){
+        else if (pieceData[1] == 3){
             blockData[0][2] = 1;
             blockData[1][2] = 1;
             blockData[2][2] = 0;
@@ -169,92 +170,92 @@ class Piece {
                 blockData[i][2] = 1;
     }
 
-    public void newPiece(int x, int y) {
+    public void newPiece(int column) {
         Random rand = new Random();
         switch(rand.nextInt(7)){
             case 0: //I
                 blocks = new Block [4];
-                    for (int i = 0; i < 4; i++)
-                        blocks[i] = new Block("\033[48;2;20;240;240m \033[0m");
+                for (int i = 0; i < 4; i++)
+                    blocks[i] = new Block("\033[48;2;20;240;240m \033[0m");
+                pieceData = new int [] {column, 0, 1};
                 blockData = new int[][] {
                     {-1, 0, 0},
                     {0, 0, 0},
                     {1, 0, 0},
-                    {2, 0, 0},
-                    {x, y, 0, 1}
+                    {2, 0, 0}
                 };
                 break;
             case 1: // Cube
                 blocks = new Block [4];
-                    for (int i = 0; i < 4; i++)
-                        blocks[i] = new Block("\033[48;2;234;236;35m \033[0m");
+                for (int i = 0; i < 4; i++)
+                    blocks[i] = new Block("\033[48;2;234;236;35m \033[0m");
+                pieceData = new int [] {column, 1, 1};
                 blockData = new int[][] {
                     {-1, 0, 0},
                     {0, 0, 0},
                     {-1, 1, 0},
-                    {0, 1, 0},
-                    {x, y, 1, 1}
+                    {0, 1, 0}
                 };
                 break;
             case 2: // T
                 blocks = new Block [4];
-                    for (int i = 0; i < 4; i++)
-                        blocks[i] = new Block("\033[48;2;180;0;158m \033[0m");
+                for (int i = 0; i < 4; i++)
+                    blocks[i] = new Block("\033[48;2;180;0;158m \033[0m");
+                pieceData = new int [] {column, 2, 1};
                 blockData = new int[][] {
                     {-1, 0, 0},
                     {0, 0, 0},
                     {1, 0, 0},
-                    {0, 1, 0},
-                    {x, y, 2, 1}
+                    {0, 1, 0}
                 };
                 break;
             case 3: // L
                 blocks = new Block [4];
-                    for (int i = 0; i < 4; i++)
-                        blocks[i] = new Block("\033[48;2;252;127;0m \033[0m");
+                for (int i = 0; i < 4; i++)
+                    blocks[i] = new Block("\033[48;2;252;127;0m \033[0m");
+                pieceData = new int [] {column, 3, 1};
                 blockData = new int[][] {
                     {-1, 0, 0},
                     {0, 0, 0},
                     {1, 0, 0},
-                    {1, -1, 0},
-                    {x, y, 3, 1}
+                    {1, -1, 0}
                 };
                 break;
             case 4: // J
                 blocks = new Block [4];
-                    for (int i = 0; i < 4; i++)
-                        blocks[i] = new Block("\033[48;2;88;51;255m \033[0m");
+                for (int i = 0; i < 4; i++)
+                    blocks[i] = new Block("\033[48;2;88;51;255m \033[0m");
+                pieceData = new int [] {column, 4, 1};
                 blockData = new int[][] {
                     {-1, -1, 0},
                     {-1, 0, 0},
                     {0, 0, 0},
-                    {1, 0, 0},
-                    {x, y, 4, 1}
+                    {1, 0, 0}
                 };
                 break;
             
             case 5: // Z
                 blocks = new Block [4];
-                    for (int i = 0; i < 4; i++)
-                        blocks[i] = new Block("\033[48;2;252;57;31m \033[0m");
+                for (int i = 0; i < 4; i++)
+                    blocks[i] = new Block("\033[48;2;252;57;31m \033[0m");
+                pieceData = new int [] {column, 5, 1};
                 blockData = new int[][] {
                     {-1, 0, 0},
                     {0, 0, 0},
                     {0, 1, 0},
-                    {1, 1, 0},
-                    {x, y, 5, 1}
+                    {1, 1, 0}
                 };
                 break;
             case 6: // S
                 blocks = new Block [4];
-                    for (int i = 0; i < 4; i++)
-                        blocks[i] = new Block("\033[48;2;49;231;34m \033[0m");
+                for (int i = 0; i < 4; i++)
+                    blocks[i] = new Block("\033[48;2;49;231;34m \033[0m");
+                pieceData = new int [] {column, 6, 1};
                 blockData = new int[][] {
                     {0, 0, 0},
                     {1, 0, 0},
                     {-1, 1, 0},
-                    {0, 1, 0},
-                    {x, y, 6, 1}
+                    {0, 1, 0}
                 };
                 break;
         }
